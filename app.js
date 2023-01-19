6176e6d72301c45958640e9ea3942566302f893a
Mohamed Farah <mohamedfarah0508@gmail.com>
2023-01-18 20:56:32 -0800

    adding routes

--- app.js
+++ app.js
@@ -1,53 +1,32 @@
+// Necessary equirements and dependencies.
 const express = require("express");
 const fs = require("fs");
-const notes = require("./db/db.json");
 const path = require("path");
-const uuid = require("uuid");
-const { DH_CHECK_P_NOT_SAFE_PRIME } = require("constants");
-
+const PORT = process.env.PORT || 3000;
 const app = express();
-var PORT = process.env.PORT || 3000;
+const notes = require("./db/db.json");
 
-// Middleware
+// Required middleware.
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-//Setting routes for APIs
-//This gets notes saved and joins it in db.json
+// Get request to pull from ap and slice() data.
 app.get("/api/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "/db/db.json"));
+  res.json(notes.slice(1));
 });
 
-// Post function to add new notes to db.json
-app.post("/api/notes", (req, res) => {
-  const notes = JSON.parse(fs.readFileSync("./db/db.json"));
-  const newNotes = req.body;
-  newNotes.id = uuid.v4();
-  notes.push(newNotes);
-  fs.writeFileSync("./db/db.json", JSON.stringify(notes));
-  res.json(notes);
+// Get request to my index file.
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
-//used for deleting notes
-app.delete("/api/notes/:id", (req, res) => {
-  const notes = JSON.parse(fs.readFileSync("./db/db.json"));
-  const delNote = notes.filter((rmvNote) => rmvNote.id !== req.params.id);
-  fs.writeFileSync("./db/db.json", JSON.stringify(delNote));
-  res.json(delNote);
+// Get request to my notes file
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
-//HTML calls
-//calls home page
-app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "/public/index.html"));
-});
-//call for notes.html
-app.get("/notes", function (req, res) {
-  res.sendFile(path.join(__dirname, "/public/notes.html"));
-});
-
-//Start listen
-app.listen(PORT, function () {
-  console.log("App listening on PORT: " + PORT);
+// This route is handling all GET requests that do not match any other routes defined previously.
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
