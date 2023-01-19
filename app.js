ea5d7c064e91d85d187549154d4949fe838921cc
Mohamed Farah <mohamedfarah0508@gmail.com>
2023-01-18 23:13:45 -0800

    test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const app = express();
 const notes = require("./db/db.json");
 
